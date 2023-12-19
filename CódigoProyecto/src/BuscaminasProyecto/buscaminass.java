c7c4be699970579d3f4fb09dcf4d5ec8b1a6d82f
María Solís <solis.rodrguez.maria@iescamas.es>
2023-12-19 19:18:42 +0100

    Añadida funcion que pinta matriz

--- CódigoProyecto/src/BuscaminasProyecto/buscaminass.java
+++ CódigoProyecto/src/BuscaminasProyecto/buscaminass.java
@@ -1,34 +1,23 @@
 package BuscaminasProyecto;
 
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
 
 public class buscaminass {
 
-	public static int [] [] generarTablero(int filas, int columnas) {
-        
-        int[][] tableroNivel = new int[filas][columnas];
-        
-        for (int i = 0; i < tableroNivel.length; i++) {
-            
-             for (int j = 0; j < tableroNivel[i].length; j ++) {
-    
-             }
-        }
-        return tableroNivel;
-        
-    }
+	
 	
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
 		System.out.println("¡Bienvenido al Buscaminas!");
 		System.out.println("Las reglas son simples, deberá seleccionar la posición de una casilla del tablero para revelar su contenido. Si es una mina, explotará, si no es una mina, podrá seguir jugando");
-		System.out.println("Presione 1, para el nivel fácil, 2 para el nivel intermedio, 3 para el nivel difícil o 4 para el nivel custom: ");
-		int nivel = sc.nextInt();
 		int minas;
 		int filas;
 		int columnas;
-		int [][] tablero;
+		int nivel;
+		int contadorVidas;
+		int i, j;
 		
 		
 			do {
@@ -42,20 +31,22 @@ public class buscaminass {
 				
 			
 					case 1:
-								
-							System.out.println(Arrays.deepToString(generarMinas(generarTablero(3,3), 3)));
+						pintaMatriz(3, 3);
 							break;
 					
 					case 2:
 					
-							System.out.println(Arrays.deepToString(generarMinas(generarTablero(5,5), 8)));
+							//System.out.println(Arrays.deepToString(generarMinas(generarTablero(5,5), 8)));
+							pintaMatriz(5, 5);
 							break;
 							
 					case 3:
 
 					
-							System.out.println(Arrays.deepToString(generarMinas(generarTablero(8,8), 15)));
+							//System.out.println(Arrays.deepToString(generarMinas(generarTablero(8,8), 15)));
+							pintaMatriz(8, 8);
 							break;
+							
 							
 					
 					case 4:
@@ -73,6 +64,7 @@ public class buscaminass {
 							minas = sc.nextInt();
 							
 							System.out.println(Arrays.deepToString(generarMinas(generarTablero(filas,columnas), minas)));
+							pintaMatriz(filas, columnas);
 							break;
 				
 					default : System.out.println("Selección incorrecta, intentelo de nuevo");
@@ -86,70 +78,46 @@ public class buscaminass {
 
 	}
 
-}
-
-	
 		public static int [] [] generarTablero(int filas, int columnas) {
-			
-			
 			int[][] tableroNivel = new int[filas][columnas];
-			
 			for (int i = 0; i < tableroNivel.length; i++) {
-				
 				 for (int j = 0; j < tableroNivel[i].length; j ++) {
-				
-				 }	
-				
+				 }		
 		}
-			
 			return tableroNivel;
 			
 		}
 		
-		
-		
-		
-		
-		
-		
-			
 		//Manu
 		public static int [] [] generarMinas(int [][] generarTablero , int minas) {
-			
-			
 			Random random = new Random();
-			
 			int [][] tablaMinas = generarTablero;
-			
-			
-			int contadorminas = 0;
-			
-				
-					 do {		
-						
-						int i1 = (int)(Math.random() * tablaMinas.length);
-							
-						int j1 = (int)(Math.random() * tablaMinas[i1].length);
-						
-						
-						
-							 if(tablaMinas[i1][j1] == 0){
-								 
-								tablaMinas[i1][j1] = 1;
-								
-								contadorminas++;
-								
-							 }
-							 
 
-							 
+			int contadorminas = 0;
+					 do {		
+						int i1 = (int)(Math.random() * tablaMinas.length);
+						int j1 = (int)(Math.random() * tablaMinas[i1].length);
+							 if(tablaMinas[i1][j1] == 0){ 
+								tablaMinas[i1][j1] = 1;
+								contadorminas++;
+							 }
 					}while(contadorminas < minas);
-					 
-					 
-			
-			
 		return tablaMinas;
-		
 		}
 		
+
+	public static char[][] pintaMatriz(int filas, int columnas){
+		char[][] tableroPintado = new char[filas][columnas];
+		for(int i=0; i< tableroPintado.length; i++){
+			 System.out.println();
+			for(int j=0; j< tableroPintado[i].length; j++){
+				tableroPintado[i][j] = 'x' ;
+				 System.out.print(tableroPintado[i][j] + " ");       
+			}
+		}
+
+		return tableroPintado;
+
+	}
 	
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # Añadida función que realiza el tablero con las dimensiones específicas de cada nivel y mitad del main realizado
 # Se han añadido dos funciones para que nos genere el tablero y las minas
 # Se añade también a la función main una comprobacion de que las minas se generan correctamente y sin repetirse según el modo de juego que elijamos.
+# Añadida la función que pinta la matriz en cada nivel.
