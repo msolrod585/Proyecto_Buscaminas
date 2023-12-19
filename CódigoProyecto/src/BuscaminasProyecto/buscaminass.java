526d9a72cf71204fc1798b4069381857855833de
María Solís <solis.rodrguez.maria@iescamas.es>
2023-12-19 21:04:00 +0100

    Añadidos codigos al nivel dificil y customizado

--- CódigoProyecto/src/BuscaminasProyecto/buscaminass.java
+++ CódigoProyecto/src/BuscaminasProyecto/buscaminass.java
@@ -6,8 +6,6 @@ import java.util.Random;
 
 public class buscaminass {
 
-	
-	
 	public static void main(String[] args) {
 		var sc = new Scanner(System.in);
 		System.out.println("¡Bienvenido al Buscaminas!");
@@ -15,12 +13,7 @@ public class buscaminass {
 		int minas;
 		int filas;
 		int columnas;
- HEAD
 		int nivel;
-		int contadorVidas;
-		int i, j;
-
-		int [][] tablero;
 		int i, j,contadorVidas;
 		int [][] tablaMinas;
 			
@@ -37,21 +30,9 @@ public class buscaminass {
 				
 			
 					case 1:
-<<<<<<< HEAD
-						pintaMatriz(3, 3);
-							break;
-					
-					case 2:
-					
-							//System.out.println(Arrays.deepToString(generarMinas(generarTablero(5,5), 8)));
-							pintaMatriz(5, 5);
-							break;
-=======
-
-							tablaMinas = generarMinas(generarTablero(3,3),3);
 						
+						tablaMinas = generarMinas(generarTablero(3,3),3);
                         contadorVidas = 5;
-                        
                         pintaMatriz(3, 3);
                         
                     do {
@@ -63,29 +44,21 @@ public class buscaminass {
                         j = sc.nextInt();
 
                         if( compruebaMinas(i, j, tablaMinas) == true ) {
-                        	
                             contadorVidas --;
-                            
                             System.out.println("Boom, ha perdido una vida");
-                            
                         }else {
-                        	
-                        	
                             System.out.println("Continúa");
+                            
                         }
                         
-
-                    }while(contadorVidas != 0);
-
-
+                    }while(contadorVidas != 0 );
                             break;
                             
+                            
+                            
 					case 2:
-					
 						tablaMinas = generarMinas(generarTablero(5,5),8);
-						
                         contadorVidas = 7;
-                        
                         pintaMatriz(5, 5);
                         
                     do {
@@ -97,90 +70,90 @@ public class buscaminass {
                         j = sc.nextInt();
 
                         if( compruebaMinas(i, j, tablaMinas) == true ) {
-                        	
                             contadorVidas --;
-                            
                             System.out.println("Boom, ha perdido una vida");
-                            
-                        }else {
-                        	
-                        	
+                        }else{
                             System.out.println("Continúa");
                         }
-                        
-
                     }while(contadorVidas != 0);
-
-
                             break;
 
-
->>>>>>> testManuel
-							
+		
 					case 3:
-
-					
-							//System.out.println(Arrays.deepToString(generarMinas(generarTablero(8,8), 15)));
 							pintaMatriz(8, 8);
-							break;
 							
-							
-					
+							tablaMinas = generarMinas(generarTablero(8,8),15);
+	                        contadorVidas = 13;
+	                        pintaMatriz(8, 8);
+	                        
+	                    do {
+	                    	
+	                        System.out.println("Introduce la primera coordenada:");
+	                        i = sc.nextInt();
+	                        
+	                        System.out.println("Introduce la segunda coordenada:");
+	                        j = sc.nextInt();
+
+	                        if( compruebaMinas(i, j, tablaMinas) == true ) {
+	                            contadorVidas --;
+	                            System.out.println("Boom, ha perdido una vida");
+	                        }else{
+	                            System.out.println("Continúa");
+	                        }
+	                    }while(contadorVidas != 0);
+	                            break;
+	                            
+	                            
 					case 4:
 			
-					
 							System.out.println("Introduce el número de filas : ");
 							filas = sc.nextInt();
-							
-							
 							System.out.println("Introduce el número de columnas : ");
 							columnas = sc.nextInt();
-							
-							
 							System.out.println("Introduce el número de minas : ");
 							minas = sc.nextInt();
 							
-							System.out.println(Arrays.deepToString(generarMinas(generarTablero(filas,columnas), minas)));
-							pintaMatriz(filas, columnas);
+							tablaMinas = generarMinas(generarTablero(filas,columnas),minas);
+	                        contadorVidas = (int)(Math.random() * 21);
+	                        pintaMatriz(filas,columnas);
+	                        System.out.println("En este nivel tienes: " + contadorVidas +" vidas.");
+							
+	                    do {
+	                        System.out.println("Introduce la primera coordenada: ");
+	                        i = sc.nextInt();
+	                        System.out.println("Introduce la segunda coordenada: ");
+	                        j = sc.nextInt();
+
+	                        if( compruebaMinas(i, j, tablaMinas) == true ) {
+	                            contadorVidas --;
+	                            System.out.println("Boom, ha perdido una vida");
+	                        }else{
+	                            System.out.println("Continúa");
+	                        }
+	                        
+	                    }while(contadorVidas != 0 );
 							break;
 				
 					default : System.out.println("Selección incorrecta, intentelo de nuevo");
 					break;
 					}
-			
-			
-			}while(nivel < 1 || nivel > 4);	
-		
-		sc.close();
+				}while(nivel < 1 || nivel > 4);	
+			  sc.close();
+			}
 
-	}
-
+	
 		public static int [] [] generarTablero(int filas, int columnas) {
 			int[][] tableroNivel = new int[filas][columnas];
 			for (int i = 0; i < tableroNivel.length; i++) {
 				 for (int j = 0; j < tableroNivel[i].length; j ++) {
 				 }		
-		}
+			}
 			return tableroNivel;
-			
-		}
+			}
 		
-<<<<<<< HEAD
-		//Manu
-=======
-		
-		
-		
-		
-		
-		
-			
-		
->>>>>>> testManuel
-		public static int [] [] generarMinas(int [][] generarTablero , int minas) {
-			Random random = new Random();
-			int [][] tablaMinas = generarTablero;
 
+		public static int [] [] generarMinas(int [][] generarTablero , int minas) {
+			int [][] tablaMinas = generarTablero;
 			int contadorminas = 0;
 					 do {		
 						int i1 = (int)(Math.random() * tablaMinas.length);
@@ -190,10 +163,10 @@ public class buscaminass {
 								contadorminas++;
 							 }
 					}while(contadorminas < minas);
-		return tablaMinas;
-		}
+			 return tablaMinas;
+			}
 		
-<<<<<<< HEAD
+
 
 	public static char[][] pintaMatriz(int filas, int columnas){
 		char[][] tableroPintado = new char[filas][columnas];
@@ -204,27 +177,16 @@ public class buscaminass {
 				 System.out.print(tableroPintado[i][j] + " ");       
 			}
 		}
-
 		return tableroPintado;
-
 	}
 	
-}
-=======
+
 		
 	public static boolean compruebaMinas(int fila ,int columna, int [][] generarMinas){
-		
 		boolean explota = false;
-		
-				
-				if (generarMinas[fila][columna] == 1) {
-					
-					
+			if (generarMinas[fila][columna] == 1) {
 					explota = true ;
-					
-				} 
-				
-				return explota;
-			}
-
->>>>>>> testManuel
+				}
+			return explota;
+		  }
+   }
